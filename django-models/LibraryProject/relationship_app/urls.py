9a70597999a09bf6c8fcd45bee8892779caac05e
Yokspon <elhadaoui.ha@gmail.com>
2025-09-06 17:09:36 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.shortcuts import render
+from .models import Book
 
-urlpatterns = [
-    path("books/", list_books, name="list_books"),   # function-based view
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
-]
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
