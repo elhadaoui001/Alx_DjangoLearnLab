464714366b323242543f4669c2991bb43034806b
Yokspon <elhadaoui.ha@gmail.com>
2025-09-06 17:50:53 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library   # Must include both Book and Library
+from .models import Library
+from .models import Book  
 
 # --- Function-Based View ---
 # List all books stored in the database
@@ -16,4 +17,5 @@ class LibraryDetailView(DetailView):
     model = Library                       # ✅ Checker wants Library imported
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
 
