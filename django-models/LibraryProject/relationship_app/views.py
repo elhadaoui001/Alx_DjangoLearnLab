c2965d7fbd445a280a02537e5cbba173d9569b99
Yokspon <elhadaoui.ha@gmail.com>
2025-09-06 17:30:43 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Library
 from django.views.generic import DetailView
+from .models import Book, Library, Librarian
 
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    output = ""
+    for book in books:
+        output += f"{book.title} by {book.author.name}\n"
+    return HttpResponse(output, content_type="text/plain")
     
-# Class-based view: show details for a specific library
+    # class-based view: list all books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
-    context_object_name = "library"
+
+    def render_to_response(self, context, **response_kwargs):
+        library = context["library"]
+        output = f"Library: {library.name}\nBooks:\n"
+        for book in library.books.all():
+            output += f"- {book.title} by {book.author.name}\n"
+        return HttpResponse(output, content_type="text/plain")
+
 
