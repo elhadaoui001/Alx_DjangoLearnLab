6ffbf8d38bdaed75d3ad875658439b5128c111b6
Yokspon <elhadaoui.ha@gmail.com>
2025-09-14 23:39:42 +0100

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,10 +1,16 @@
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date']
-
-class BookSearchForm(forms.Form):
-    q = forms.CharField(max_length=255, required=False)
+class ExampleForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your name'})
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Enter your message'}),
+        required=True
+    )
