d34ab57cacb5aade24b4869afbd5f665003b3fb7
Yokspon <elhadaoui.ha@gmail.com>
2025-09-14 23:26:56 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -22,7 +22,26 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
+    # LibraryProject/bookshelf/models.py
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
     def __str__(self):
         return self.username
+
 
 
