8d87ce2d9b222eaf3a6c91c90e583bb98feee239
Yokspon <elhadaoui.ha@gmail.com>
2025-09-14 23:28:08 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+# LibraryProject/bookshelf/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        Book.objects.create(title=title, author=author)
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_form.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.save()
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_form.html', {'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    book.delete()
+    return redirect('book_list')
+
