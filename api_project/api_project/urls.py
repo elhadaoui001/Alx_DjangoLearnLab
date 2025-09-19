956f424d0f9500f6fb7ae982ba15d7e50422ade1
Yokspon <elhadaoui.ha@gmail.com>
2025-09-19 22:07:55 +0100

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from .views import BookList
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # include API app routes
 ]
+
+
 
 
