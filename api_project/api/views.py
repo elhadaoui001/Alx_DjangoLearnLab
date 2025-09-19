014a2bbb71400101f078764648b302afd1825b58
Yokspon <elhadaoui.ha@gmail.com>
2025-09-19 22:46:11 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -13,4 +13,11 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+from rest_framework import viewsets, permissions
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only logged-in users with a token can access
+
 
