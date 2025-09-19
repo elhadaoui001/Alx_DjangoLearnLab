bce12c93a2006bbec6e2961e6e12d7e08f08b134
Yokspon <elhadaoui.ha@gmail.com>
2025-09-19 22:43:14 +0100

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -22,6 +22,14 @@ urlpatterns = [
     path('api/', include('api.urls')),  # include API app routes
 ]
 
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),  # login to get token
+]
+
 
 
 
