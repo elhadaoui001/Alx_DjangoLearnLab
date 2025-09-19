8c75fa2c26c0a1a49b3ac21268e2ea59a2aa4418
Yokspon <elhadaoui.ha@gmail.com>
2025-09-19 22:37:21 +0100

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # optional (useful for browsing API in dev)
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Default: only authenticated users can access
+    ],
+}
